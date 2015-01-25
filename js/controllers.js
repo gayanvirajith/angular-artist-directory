33b74a692ccd22cc1153f0192b7150807f5bdf0e
Gayan Virajith <gayanvirajith@gmail.com>
2015-01-25 21:03:54 +0530

    Add search filters using angular orderBy filter

--- js/controllers.js
+++ js/controllers.js
@@ -2,7 +2,7 @@ app.controller('DirectoryController', ['$http', function($http) {
     
   var directory = this;
   directory.artists = [];
-
+  this.artistOrder = "name";
   $http.get('js/data.json').success(function(data) {
     directory.artists = data;
   });
